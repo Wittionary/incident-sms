4dd58416c24749d7a129ef2a908146aa31b48b4c
Witt Allen <Wittionary@users.noreply.github.com>
2019-04-08 15:03:29 -0500

    Next steps: editable field in tk that you can SMS

--- incident-sms.py
+++ incident-sms.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+from twilio.rest import Client
+import json
+
+# TODO:
+# Contact list (hardcode at first)
+# Format/template of text (hardcode at first)
+# SMS provider - so far just Twilio
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -6,20 +13,47 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        self.twilioClient = self.prepTwilio()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.twilioButton = tk.Button(self)
+        self.twilioButton["text"] = "Twilio SMS"
+        self.twilioButton["command"] = self.sendSMS
+        self.twilioButton.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.smsContent = tk.Text(self)
+        self.smsContent.pack(expand=1, side="left")
+
+    def prepTwilio(self):
+        with open("config.json") as json_data_file:
+            config = json.load(json_data_file)
+
+        accountSid = config['twilio']['accountSID']
+        authToken = config['twilio']['authToken']
+        client = Client(accountSid, authToken)
+        return client
+       
+
+    def sendSMS(self, self.smsContent.get): # needs to take in SMS provider and format/template as params in future
+        message = self.twilioClient.messages \
+                .create(
+                     body="Header:\n \
+                        device1, device2, device3 \n\n \
+                        Datasource/datapoint:\n \
+                        ping alert host status\n\n \
+                        Value:\n \
+                        9001\n\n \
+                        Et cetera",
+                     from_='+1devphone',
+                     to='+1mycellphone'
+                 )
+        print(message.sid)
 
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
+
