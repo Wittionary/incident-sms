9ccf44daacc4a9ccc2f70ee5eb982bc24155e480
Witt Allen <Wittionary@users.noreply.github.com>
2019-04-10 15:01:24 -0500

    MVP is basically there

--- incident-sms.py
+++ incident-sms.py
@@ -13,11 +13,14 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.title("Incident SMS")
         self.pack()
         self.create_widgets()
         self.twilioClient = self.prepTwilio()
 
     def create_widgets(self):
+        # Make widgets dynamic after figuring out format/template file
+        # Resource: https://www.python-course.eu/tkinter_layout_management.php
         #       col 0   |   col 1   | col 2
         # row 0         |           |
         # -----------------------------------
@@ -28,30 +31,45 @@ class Application(tk.Frame):
         # row 3         |           |
         # -----------------------------------
 
+        # send SMS button
+        self.twilioButton = tk.Button(self,text="Twilio SMS", fg="red", command=self.confirmSMSmessage)
+        self.twilioButton.grid(row=10,column=1)
 
-        self.twilioButton = tk.Button(self)
-        self.twilioButton["text"] = "Twilio SMS"
-        self.twilioButton["command"] = self.sendSMS
-        self.twilioButton.grid(row=4,column=1)
-
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.grid(row=4,column=2)
+        # quit button
+        self.quit = tk.Button(self, text="QUIT", command=self.master.destroy)
+        self.quit.grid(row=10,column=2)
 
         # label 1
-        self.labelDevice = tk.Label(self, text="Device:")
+        self.labelDevice = tk.Label(self, text="Device:",justify=tk.LEFT)
         self.labelDevice.grid(row=0,column=0)
 
         # text field 1
-        self.smsContent = tk.Entry(self)
-        self.smsContent.grid(row=0,column=1)
+        self.textDevice = tk.Entry(self)
+        self.textDevice.grid(row=0,column=1)
 
         # label 2
-        self.labelError = tk.Label(self, text="Error or Datapoint & Value:")
+        self.labelError = tk.Label(self, text="Error or Datapoint & Value:",justify=tk.LEFT)
         self.labelError.grid(row=1,column=0)
 
+        # text field 2
+        self.textError = tk.Entry(self)
+        self.textError.grid(row=1,column=1)
+
         # label 3
-        self.labelTime = tk.Label(self, text="Time Began:")
+        self.labelTime = tk.Label(self, text="Time Began:",justify=tk.LEFT)
         self.labelTime.grid(row=2,column=0)
+
+        # text field 3
+        self.textTime = tk.Entry(self)
+        self.textTime.grid(row=2,column=1)
+
+        # label 4
+        self.labelAssociates = tk.Label(self, text="Associates Contacted:",justify=tk.LEFT)
+        self.labelAssociates.grid(row=3,column=0)
+
+        # text field 4
+        self.textAssociates = tk.Entry(self)
+        self.textAssociates.grid(row=3,column=1)
 
     def prepTwilio(self):
         with open("config.json") as json_data_file:
@@ -62,6 +80,14 @@ class Application(tk.Frame):
         client = Client(accountSid, authToken)
         return client
        
+    def createSMS(self):
+        # Message = label text + \n + text.get() + \n\n
+        message = f"Device:\n{self.textDevice.get()}\n\n"
+        message += f"Error or Datapoint & Value:\n{self.textError.get()}\n\n"
+        message += f"Time Began:\n{self.textTime.get()}\n\n"
+        message += f"Associates Contacted:\n{self.textAssociates.get()}\n\n"
+        
+        return message
 
     def sendSMS(self): # needs to take in SMS provider and format/template as params in future
         with open("config.json") as json_data_file:
@@ -72,11 +98,22 @@ class Application(tk.Frame):
         
         message = self.twilioClient.messages \
                 .create(
-                     body=self.smsContent.get(),
+                     body=self.createSMS(),
                      from_=fromNumber,
                      to=toNumber
                  )
         print(message.sid)
+              
+    def confirmSMSmessage(self):
+        popup = tk.Tk()
+        popup.title("Are you sure?")
+        labelSMS = tk.Label(popup, text=self.createSMS())
+        labelSMS.pack(side="top")
+        buttonConfirm = tk.Button(popup, text="Send", command = self.sendSMS())
+        buttonConfirm.pack(side=tk.LEFT)
+        buttonCancel = tk.Button(popup, text="Cancel", command=popup.destroy)
+        buttonCancel.pack(side=tk.RIGHT)
+        popup.mainloop()
 
 root = tk.Tk()
 app = Application(master=root)
