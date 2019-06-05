e0c7c9ed4872b7c2683e3a19c78d6a06c81aee51
Witt Allen <Wittionary@users.noreply.github.com>
2019-06-05 13:39:03 -0500

    Working with errors/exceptions

--- incident-sms.py
+++ incident-sms.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from twilio.rest import Client
 import json
-from flask import Flask
+from flask import Flask, render_template, request
 
 # TODO:
 # [DONE] Contact list (hardcode at first)
@@ -20,139 +20,69 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
 
 @app.route("/witt")
 def witt():
-    return "Hell, Witt"
+    print("Hello, Witt")
 
-if __name__ == "__incident-sms__":
-    app.run(debug=True)
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.master.title("Incident SMS")
-        self.pack()
-        self.create_widgets()
-        self.twilioClient = self.prepTwilio()
-
-    def create_widgets(self):
-        # Make widgets dynamic after figuring out format/template file
-        # Resource: https://www.python-course.eu/tkinter_layout_management.php
-        #       col 0   |   col 1   | col 2
-        # row 0         |           |
-        # -----------------------------------
-        # row 1         |           |
-        # -----------------------------------
-        # row 2         |           |
-        # -----------------------------------
-        # row 3         |           |
-        # -----------------------------------
-
-        # send SMS button
-        self.twilioButton = tk.Button(self,text="Send SMS", fg="red", command=self.confirmSMSmessage)
-        self.twilioButton.grid(row=10,column=1)
-
-        # quit button
-        self.quit = tk.Button(self, text="QUIT", command=self.master.destroy)
-        self.quit.grid(row=10,column=2)
-
-        # label 1
-        self.labelDevice = tk.Label(self, text="Device:",justify=tk.LEFT)
-        self.labelDevice.grid(row=0,column=0)
-
-        # text field 1
-        self.textDevice = tk.Entry(self, width=60)
-        self.textDevice.grid(row=0,column=1)
-
-        # label 2
-        self.labelError = tk.Label(self, text="Error or Datapoint & Value:",justify=tk.LEFT)
-        self.labelError.grid(row=1,column=0)
-
-        # text field 2
-        self.textError = tk.Text(self, width=60)
-        self.textError.grid(row=1,column=1)
-
-        # label 3
-        self.labelTime = tk.Label(self, text="Time Began:",justify=tk.LEFT)
-        self.labelTime.grid(row=2,column=0)
-
-        # text field 3
-        self.textTime = tk.Entry(self, width=60)
-        self.textTime.grid(row=2,column=1)
-
-        # label 4
-        self.labelAssociates = tk.Label(self, text="Associates Contacted:",justify=tk.LEFT)
-        self.labelAssociates.grid(row=3,column=0)
-
-        # text field 4
-        self.textAssociates = tk.Entry(self, width=60)
-        self.textAssociates.grid(row=3,column=1)
-
-    def prepTwilio(self):
-        try:
-            with open("config.json") as json_data_file:
-                config = json.load(json_data_file)
-        except FileNotFoundError:
-            print("Config file not found.")
-            return
-
-        accountSid = config['twilio']['accountSID']
-        authToken = config['twilio']['authToken']
-        client = Client(accountSid, authToken)
-        return client
-       
-    def createSMS(self):
-        # Message = label text + \n + text.get() + \n\n
-        message = f"Device:\n{self.textDevice.get()}\n\n"
-        message += f"Error or Datapoint & Value:\n{self.textError.get(1.0, 50.0)}\n"
-        message += f"Time Began:\n{self.textTime.get()}\n\n"
-        message += f"Associates Contacted:\n{self.textAssociates.get()}\n\n"
-        message += ("**Please join the Conference Bridge**\n"
-                    "Phone Number: 1-240-454-0879\n"
-                    "Attendee access code: 93008961\n"
-                    "Mobile-friendly: 240-454-0879,,,93008961#,,,#")
-        
-        return message
-
-    def sendSMS(self): # needs to take in SMS provider and format/template as params in future
+def prepTwilio():
+    try:
         with open("config.json") as json_data_file:
             config = json.load(json_data_file)
+    except FileNotFoundError:
+        print("Config file not found.")
+        return
 
-        fromNumber = config['twilio']['fromNumber']
+    accountSid = config['twilio']['accountSID']
+    authToken = config['twilio']['authToken']
+    client = Client(accountSid, authToken)
+    return client
 
-        with open("clients.json") as json_data_file:
-            clients = json.load(json_data_file)
+def createSMS():
+    # Message = label text + \n + text.get() + \n\n
+    message = f"Device:\n{request.form['device']}\n\n"
+    message += f"Error or Datapoint & Value:\n{request.form['errorinfo']}\n\n"
+    message += f"Time Began:\n{request.form['timebegan']}\n\n"
+    message += f"Associates Contacted:\n{request.form['associatescontacted']}\n\n"
+    message += ("**Please join the Conference Bridge**\n"
+                "Phone Number: 1-240-454-0879\n"
+                "Attendee access code: 93008961\n"
+                "Mobile-friendly: 240-454-0879,,,93008961#,,,#")
+    
+    return message
 
-        contacts = clients['Test Client']['contacts']
-       
-        for contact in contacts.values():
-            toNumber = contact
-        
-            message = self.twilioClient.messages \
+@app.route('/sendSMS', methods=['POST'])
+def sendSMS(): # needs to take in SMS provider and format/template as params in future
+    twilioClient = prepTwilio()
+
+    with open("config.json") as json_data_file:
+        config = json.load(json_data_file)
+
+    fromNumber = config['twilio']['fromNumber']
+
+    with open("clients.json") as json_data_file:
+        clients = json.load(json_data_file)
+
+    contacts = clients['Test Client']['contacts']
+    
+    for contact in contacts.values():
+        toNumber = contact
+    
+        try:
+            message = twilioClient.messages \
                     .create(
-                        body=self.createSMS(),
+                        body=createSMS(),
                         from_=fromNumber,
                         to=toNumber
                     )
-            
-            print(message.sid)
+        except twilio.base.exceptions.TwilioRestException as err:
+            print(f"Unexpected error: {err}")
+
+        print(message.sid)
               
-    def confirmSMSmessage(self):
-        popup = tk.Tk()
-        popup.title("Are you sure?")
-        labelSMS = tk.Label(popup, text=self.createSMS())
-        labelSMS.pack(side="top")
-        buttonConfirm = tk.Button(popup, text="Confirm", fg="red", command = self.sendSMS)
-        buttonConfirm.pack(side=tk.LEFT)
-        buttonCancel = tk.Button(popup, text="Cancel", command=popup.destroy)
-        buttonCancel.pack(side=tk.RIGHT)
-        popup.mainloop()
+    #def confirmSMSmessage(self):
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
-
+if __name__ == "__main__":
+    app.run(debug=True)
